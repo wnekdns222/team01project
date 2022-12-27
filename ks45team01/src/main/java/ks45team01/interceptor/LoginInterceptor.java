4aa44b908918822b53d3879ffbfca610060fa3cb
ECS <ECS@DESKTOP-048HU3O>
2022-12-27 09:14:13 +0900

    [project jhw] 팀프로젝트 컨트롤러 설정 및 프로젝트 화면 인서트 리스트

---
+++ ks45team01/src/main/java/ks45team01/interceptor/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package ks45team01.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+		// 레벨 권한 별 검사
+		// 메뉴 세션 셋팅
+		request.getSession().setAttribute("LEVEL_MENU", handler);
+		
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+}
