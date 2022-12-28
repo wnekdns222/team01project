3844040028e546a0728a8269fd9e14da30f52a91
ECS <ECS@DESKTOP-048HU3O>
2022-12-28 17:31:43 +0900

    [team01project jsh] 1.수정: sidebar, community controller,calender controller 추가: community, calender 2.파일 이름 : community, calender .html

--- ks45team01/src/main/java/ks45team01/controller/CalenderController.java
+++ ks45team01/src/main/java/ks45team01/controller/CalenderController.java
@@ -1,5 +1,18 @@
 package ks45team01.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/calender")
 public class CalenderController {
+	//캘린더
+		@GetMapping("calender/scheduel")
+		public String getScheduel(Model model) {   
+			model.addAttribute("title", "메인화면");
+			
+			return "calender/scheduel";
+		}
 
 }
