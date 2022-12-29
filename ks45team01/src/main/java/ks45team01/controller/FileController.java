50ec17b54f2848a96c41a736c3619a732934783e
ECS <ECS@DESKTOP-048HU3O>
2022-12-29 16:31:17 +0900

    [team01project rjy] 1. 수정 2. 추가 파일공유 관련 화면 생성, 목록, 상세보기 작업 완료했습니다.

--- ks45team01/src/main/java/ks45team01/controller/FileController.java
+++ ks45team01/src/main/java/ks45team01/controller/FileController.java
@@ -9,6 +9,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/file")
 public class FileController {
 	
+	@GetMapping("/fileUpdate")
+	public String fileUpdate(Model model) {
+		
+		model.addAttribute("title", "파일수정");
+		
+		return "file/fileUpdate";
+	}
+	
+	@GetMapping("/fileView")
+	public String fileView(Model model) {
+		
+		model.addAttribute("title", "파일상세보기");
+		
+		return "file/fileView";
+	}
+	
+	/**
+	 * 파일 등록
+	 * @param model
+	 * @return
+	 */
+	
+	@GetMapping("/addFile")
+	public String addFile(Model model) {
+		
+		model.addAttribute("title", "파일등록");
+		
+		return "file/addFile";
+	}
+	
+	/**
+	 * 파일 목록 
+	 * @param model
+	 * @return
+	 */
+	
 	@GetMapping("fileList")
 	public String fileList(Model model) {
 		
