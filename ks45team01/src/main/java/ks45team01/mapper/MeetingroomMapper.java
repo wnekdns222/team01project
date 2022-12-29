acc5226601913d67baef788fa7ef406b2238f30c
ECS <ECS@DESKTOP-048HU3O>
2022-12-29 11:37:08 +0900

    [team01project jmj] 수정 :AdminReservationController.java 생성 :Meetingroom.java 생성 :MeetingroomMapper.java 생성 :MeetingroomService.java 수정 :sideBar.html

---
+++ ks45team01/src/main/java/ks45team01/mapper/MeetingroomMapper.java
@@ -0,0 +1,10 @@
+package ks45team01.mapper;
+
+import java.util.List;
+
+import ks45team01.dto.Meetingroom;
+
+public interface MeetingroomMapper {
+
+	public List<Meetingroom> getmeetingroomList();
+}
---
+++ ks45team01/src/main/java/ks45team01/service/MeetingroomService.java
@@ -0,0 +1,12 @@
+package ks45team01.service;
+
+import org.springframework.stereotype.Service;
+
+import ks45team01.mapper.MeetingroomMapper;
+
+@Service
+public class MeetingroomService {
+
+	
+	
+}
