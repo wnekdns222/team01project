31289c123e8167c1d58afd14e030c8ab97ef0933
ECS <ECS@DESKTOP-048HU3O>
2023-01-03 17:28:52 +0900

    [멤버레벨권한 맵퍼및 서비스 컨트롤러 수정,프로젝트 홈 화면 수정 jhw] 추가 ks45team01/src/main/java/ks45team01/unity/dto/MemberLevel.java ks45team01/src/main/java/ks45team01/unity/mapper/MemberLevelMapper.java ks45team01/src/main/resources/mapper/MemberLevelMapper.xml ks45team01/src/main/java/ks45team01/unity/service/MemberLevelService.java 수정 ks45team01/src/main/resources/templates/member/member_level_list.html ks45team01/src/main/java/ks45team01/unity/worker/controller/MemberController.java ks45team01/src/main/resources/templates/project/project_home.html

---
+++ ks45team01/src/main/java/ks45team01/unity/dto/MemberLevel.java
@@ -0,0 +1,182 @@
+package ks45team01.unity.dto;
+
+public class MemberLevel {
+	private String levelCode;
+	private String memberNum;
+	private String memberAdminLevel;
+	private String memberUserLevel;
+	private String loginLevel;
+	private String projectLevel;
+	private String departWorkInfoLevel;
+	private String calenderCategoryLevel;
+	private String communityLevel;
+	private String departVacationInfoLevel;
+	private String allVactionInfoLevel;
+	private String allWorkInfoLevel;
+	private String unusualWorkInsertLevel;
+	private String approvalLevel;
+	private String majorCategoryLevel;
+	private String workVavationSettingLevel;
+	private String approvalCateLevel;
+	private String workVategoryLevel;
+	private String fileCategoryLevel;
+	public String getLevelCode() {
+		return levelCode;
+	}
+	public void setLevelCode(String levelCode) {
+		this.levelCode = levelCode;
+	}
+	public String getMemberNum() {
+		return memberNum;
+	}
+	public void setMemberNum(String memberNum) {
+		this.memberNum = memberNum;
+	}
+	public String getMemberAdminLevel() {
+		return memberAdminLevel;
+	}
+	public void setMemberAdminLevel(String memberAdminLevel) {
+		this.memberAdminLevel = memberAdminLevel;
+	}
+	public String getMemberUserLevel() {
+		return memberUserLevel;
+	}
+	public void setMemberUserLevel(String memberUserLevel) {
+		this.memberUserLevel = memberUserLevel;
+	}
+	public String getLoginLevel() {
+		return loginLevel;
+	}
+	public void setLoginLevel(String loginLevel) {
+		this.loginLevel = loginLevel;
+	}
+	public String getProject_level() {
+		return projectLevel;
+	}
+	public void setProject_level(String project_level) {
+		this.projectLevel = project_level;
+	}
+	public String getDepartWorkInfoLevel() {
+		return departWorkInfoLevel;
+	}
+	public void setDepartWorkInfoLevel(String departWorkInfoLevel) {
+		this.departWorkInfoLevel = departWorkInfoLevel;
+	}
+	public String getCalenderCategoryLevel() {
+		return calenderCategoryLevel;
+	}
+	public void setCalenderCategoryLevel(String calenderCategoryLevel) {
+		this.calenderCategoryLevel = calenderCategoryLevel;
+	}
+	public String getCommunityLevel() {
+		return communityLevel;
+	}
+	public void setCommunityLevel(String communityLevel) {
+		this.communityLevel = communityLevel;
+	}
+	public String getDepartVacationInfoLevel() {
+		return departVacationInfoLevel;
+	}
+	public void setDepartVacationInfoLevel(String departVacationInfoLevel) {
+		this.departVacationInfoLevel = departVacationInfoLevel;
+	}
+	public String getAllVactionInfoLevel() {
+		return allVactionInfoLevel;
+	}
+	public void setAllVactionInfoLevel(String allVactionInfoLevel) {
+		this.allVactionInfoLevel = allVactionInfoLevel;
+	}
+	public String getAllWorkInfoLevel() {
+		return allWorkInfoLevel;
+	}
+	public void setAllWorkInfoLevel(String allWorkInfoLevel) {
+		this.allWorkInfoLevel = allWorkInfoLevel;
+	}
+	public String getUnusualWorkInsertLevel() {
+		return unusualWorkInsertLevel;
+	}
+	public void setUnusualWorkInsertLevel(String unusualWorkInsertLevel) {
+		this.unusualWorkInsertLevel = unusualWorkInsertLevel;
+	}
+	public String getApprovalLevel() {
+		return approvalLevel;
+	}
+	public void setApprovalLevel(String approvalLevel) {
+		this.approvalLevel = approvalLevel;
+	}
+	public String getMajorCategoryLevel() {
+		return majorCategoryLevel;
+	}
+	public void setMajorCategoryLevel(String majorCategoryLevel) {
+		this.majorCategoryLevel = majorCategoryLevel;
+	}
+	public String getWorkVavationSettingLevel() {
+		return workVavationSettingLevel;
+	}
+	public void setWorkVavationSettingLevel(String workVavationSettingLevel) {
+		this.workVavationSettingLevel = workVavationSettingLevel;
+	}
+	public String getApprovalCateLevel() {
+		return approvalCateLevel;
+	}
+	public void setApprovalCateLevel(String approvalCateLevel) {
+		this.approvalCateLevel = approvalCateLevel;
+	}
+	public String getWorkVategoryLevel() {
+		return workVategoryLevel;
+	}
+	public void setWorkVategoryLevel(String workVategoryLevel) {
+		this.workVategoryLevel = workVategoryLevel;
+	}
+	public String getFileCategoryLevel() {
+		return fileCategoryLevel;
+	}
+	public void setFileCategoryLevel(String fileCategoryLevel) {
+		this.fileCategoryLevel = fileCategoryLevel;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("MemberLevel [levelCode=");
+		builder.append(levelCode);
+		builder.append(", memberNum=");
+		builder.append(memberNum);
+		builder.append(", memberAdminLevel=");
+		builder.append(memberAdminLevel);
+		builder.append(", memberUserLevel=");
+		builder.append(memberUserLevel);
+		builder.append(", loginLevel=");
+		builder.append(loginLevel);
+		builder.append(", project_level=");
+		builder.append(projectLevel);
+		builder.append(", departWorkInfoLevel=");
+		builder.append(departWorkInfoLevel);
+		builder.append(", calenderCategoryLevel=");
+		builder.append(calenderCategoryLevel);
+		builder.append(", communityLevel=");
+		builder.append(communityLevel);
+		builder.append(", departVacationInfoLevel=");
+		builder.append(departVacationInfoLevel);
+		builder.append(", allVactionInfoLevel=");
+		builder.append(allVactionInfoLevel);
+		builder.append(", allWorkInfoLevel=");
+		builder.append(allWorkInfoLevel);
+		builder.append(", unusualWorkInsertLevel=");
+		builder.append(unusualWorkInsertLevel);
+		builder.append(", approvalLevel=");
+		builder.append(approvalLevel);
+		builder.append(", majorCategoryLevel=");
+		builder.append(majorCategoryLevel);
+		builder.append(", workVavationSettingLevel=");
+		builder.append(workVavationSettingLevel);
+		builder.append(", approvalCateLevel=");
+		builder.append(approvalCateLevel);
+		builder.append(", workVategoryLevel=");
+		builder.append(workVategoryLevel);
+		builder.append(", fileCategoryLevel=");
+		builder.append(fileCategoryLevel);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}
