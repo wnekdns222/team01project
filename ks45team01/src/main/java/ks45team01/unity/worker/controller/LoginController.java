9e3c238c6f68b33e5d24cf51d59d01af23189fd5
ECS <ECS@DESKTOP-048HU3O>
2023-01-03 15:51:28 +0900

    [사이드바 경로 수정 및 로그인 화면,회원가입 화면 수정 jsh] 화면 및 경로수정 ks45team01/src/main/resources/templates/community/boardInsert.html ks45team01/src/main/resources/templates/calender/calenderPersonalList.html ks45team01/src/main/resources/templates/login/login.html ks45team01/src/main/java/ks45team01/unity/worker/controller/LoginController.java ks45team01/src/main/resources/templates/login/loginInsert.html ks45team01/src/main/resources/templates/fragments/sideBar.html

--- ks45team01/src/main/java/ks45team01/unity/worker/controller/LoginController.java
+++ ks45team01/src/main/java/ks45team01/unity/worker/controller/LoginController.java
@@ -8,11 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/Login")
 public class LoginController {
-	//로그인 화면
+			//로그인 화면
 			@GetMapping("/loginMain")
 			public String getLogin(Model model) {   
 				model.addAttribute("title", "로그인화면");
 				
 				return "login/login";
+			}
+			//로그인 화면
+			@GetMapping("/loginInsert")
+			public String getLoginInsert(Model model) {   
+				model.addAttribute("title", "회원가입화면");
+				
+				return "login/loginInsert";
 			}
 }
