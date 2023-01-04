925923b590b43cc75f82edf9898fdc07f0aa19ed
ECS <ECS@DESKTOP-048HU3O>
2023-01-04 17:28:48 +0900

    [예약 화면 수정 jmj] reservation/meetingroom_reservation_insert.html reservation/meetingroom_reservation_list.html controller/ReservationController.java mapper/ReservationMapper.java mapper/ReservationMapper.xml service/ReservationService.java

--- ks45team01/src/main/java/ks45team01/unity/mapper/ReservationMapper.java
+++ ks45team01/src/main/java/ks45team01/unity/mapper/ReservationMapper.java
@@ -2,16 +2,15 @@ package ks45team01.unity.mapper;
 
 import java.util.List;
 
+
 import org.apache.ibatis.annotations.Mapper;
 
 import ks45team01.unity.dto.Meetingroom;
-import ks45team01.unity.dto.Reservation;
 
 @Mapper
 public interface ReservationMapper {
 
 	List<Meetingroom> getMeetingroomList();
-	
-	List<Reservation> getReservationList();
+
 	
 }
--- ks45team01/src/main/java/ks45team01/unity/worker/controller/ReservationController.java
+++ ks45team01/src/main/java/ks45team01/unity/worker/controller/ReservationController.java
@@ -2,12 +2,14 @@ package ks45team01.unity.worker.controller;
 
 import java.util.List;
 
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import ks45team01.unity.dto.Reservation;
+import ks45team01.unity.dto.Meetingroom;
 import ks45team01.unity.service.ReservationService;
 
 @Controller
@@ -23,19 +25,25 @@ public class ReservationController {
 	
 	//예약 첫 화면
 	@GetMapping("/meetingroomReservationList")
-	public String getMeetingroomReservationList(Model model) {
+	public String MeetingroomList(Model model) {
 			
-		List<Reservation> reservationList = reservationService.getReservationList();
+		List<Meetingroom> meetingroomList = reservationService.getMeetingroomList();
 		
 		model.addAttribute("title", "회의실 목록");
-		model.addAttribute("reservationList", reservationList);
+		model.addAttribute("meetingroomList", meetingroomList);
 		
 		return "/reservation/meetingroom_reservation_list";
 	}
 	
 	//예약하기 화면
 	@GetMapping("/meetingroomReservationInsert")
-	public String MeetingroomReservationInsert() {
+	public String MeetingroomReservationInsert(Model model) {
+		
+		List<Meetingroom> meetingroomList = reservationService.getMeetingroomList();
+		
+		model.addAttribute("title", "회의실 목록");
+		model.addAttribute("meetingroomList", meetingroomList);
+		
 		
 		return "/reservation/meetingroom_reservation_insert";
 	}
