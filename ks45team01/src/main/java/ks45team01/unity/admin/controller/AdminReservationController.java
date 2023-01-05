5f4188caae6114d4057e25e09c5c071461b22cf4
ECS <ECS@DESKTOP-048HU3O>
2023-01-05 17:26:38 +0900

    [예약 회의실 관리 페이지 수정삭제처리 jmj]
    controller/AdminReservationController.java
    reservation/meetingroom_list.html
    reservation/meetingroom_modify.html
    reservation/meetingroom_reservation_insert.html
    reservation/meetingroom_reservation_list.html
    reservation/meetingroom_reservation_modify.html
    mapper/ReservationMapper.java
    mapper/ReservationMapper.xml
    service/ReservationService.java

--- ks45team01/src/main/java/ks45team01/unity/admin/controller/AdminReservationController.java
+++ ks45team01/src/main/java/ks45team01/unity/admin/controller/AdminReservationController.java
@@ -2,11 +2,14 @@ package ks45team01.unity.admin.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ks45team01.unity.dto.Meetingroom;
 import ks45team01.unity.service.ReservationService;
@@ -18,6 +21,9 @@ import ks45team01.unity.service.ReservationService;
 public class AdminReservationController {
 
 	
+	private static final Logger log = LoggerFactory.getLogger(AdminReservationController.class);
+
+	
 	private final ReservationService reservationService;
 	
 	public AdminReservationController(ReservationService reservationService){
@@ -26,7 +32,7 @@ public class AdminReservationController {
 	
 	//회의실 관리 첫 화면
 	@GetMapping("/meetingroomList")
-	public String MeetingroomList(Model model) {
+	public String meetingroomList(Model model) {
 		
 		List<Meetingroom> meetingroomList = reservationService.getMeetingroomList();
 		
@@ -36,5 +42,37 @@ public class AdminReservationController {
 
 		return "reservation/meetingroom_list";
 	}
+	
+	//회의실 수정
+		@GetMapping("/meetingroomModify")
+		public String modifyMeetingroom(@RequestParam(value="meetNum", required=false) String meetNum,Model model) {
+			
+			log.info("meetingroom: {}", meetNum);
+			Meetingroom meetingroom = reservationService.getMeetingroomById(meetNum);
+			model.addAttribute("meetingroom", meetingroom);
+
+			return "reservation/meetingroom_modify";
+		}
+		
+	//회의실 수정
+		@PostMapping("/meetingroomModify")
+		public String modifyMeetingroom(Meetingroom meetingroom) {
+			
+			log.info("meetingroom: {}", meetingroom);
+			reservationService.modifyMeetingroom(meetingroom);
+			
+			return "redirect:/reservation/meetingroomList";
+		
+		
+		}
+	//회의실 삭제
+		@GetMapping("/meetingroomDelete")
+		public String deleteMeetingroom(Meetingroom meetingroom) {
+			
+			reservationService.deleteMeetingroom(meetingroom);
+			
+			return "redirect:/reservation/meetingroomList";
+			
+		}
 	
 }
