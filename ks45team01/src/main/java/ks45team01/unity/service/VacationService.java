6155e06b8052e9465a69c4176e4b6f307adc88b7
ASUS <ASUS@DESKTOP-T50VO8O>
2023-01-10 09:08:14 +0900

    [휴가 설정 수정 및 입력 파일 생성,수정 kjy] 수정파일 AdminWorkController.java VacationCategory.java VacationMapper.xml VacationService.java VacationSort.java
    생성파일
    vacation_category_insert.html
    vacation_category_modify.html
    vacation_sort_insert.html
    vacation_sort_modify.html
    vacation_type_insert.html
    vacation_type_modify.html
    vacation_variety_insert.html
    vacation_variety_list.html
    vacation_variety_modify.html

--- ks45team01/src/main/java/ks45team01/unity/service/VacationService.java
+++ ks45team01/src/main/java/ks45team01/unity/service/VacationService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import ks45team01.unity.dto.VacationCategory;
 import ks45team01.unity.dto.VacationSort;
@@ -13,6 +14,7 @@ import ks45team01.unity.dto.VacationType;
 import ks45team01.unity.mapper.VacationMapper;
 
 @Service
+@Transactional
 public class VacationService {
 
 	
@@ -38,6 +40,107 @@ public class VacationService {
 		variety.put("standard", standard);
 		return variety;
 	}
+	/**
+	 * 휴가 대분류 비동기 수정
+	 */
+	public void updateVacationCategoryA(String vacationCategoryNum, String vacationUse) {
+		
+		vacationMapper.updateVacationCategoryA();
+	}
+	/**
+	 * 휴가 중분류 비동기 수정
+	 */
+	public void updateVacationSortA(String vacationSortNum, String vacationUse) {
+		
+		vacationMapper.updateVacationSortA();
+	}
+	/**
+	 * 휴가 종류 비동기 수정
+	 */
+	public void updateVacationTypeA(String vacationTypeNum, String vacationUse) {
+		
+		vacationMapper.updateVacationTypeA();
+	}
+	/**
+	 * 휴가 기준 비동기 수정
+	 */
+	public void updateVacationStandardA(String vacationStandardNum, String vacationStandardUse) {
+		
+		vacationMapper.updateVacationStandardA();
+	}
+	/**
+	 * 휴가 종류 등록
+	 * 
+	 */
+	public void addVacationType(VacationType vacationType) {
+		
+		String vacationTypeNum =vacationMapper.getCommonNewCode("tb_vacation_type", "vacation_type_num");
+		vacationType.setVacationTypeNum(vacationTypeNum);
+		vacationMapper.addVacationType(vacationType);
+		
+	}
+	/**
+	 * 특정 휴가 종류 조회
+	 * 
+	 */
+	public VacationType getVacationTypeByNum(String vacationTypeNum) {
+			VacationType vacationType = vacationMapper.getVacationTypeByNum(vacationTypeNum);
+			
+		return vacationType;
+	}
+	/**
+	 * 특정 휴가 종류 수정
+	 * 
+	 */
+	public void updateVacationType(VacationType vacationType) {
+		vacationMapper.updateVacationType(vacationType);
 	
-	
-}
+	}
+	/**
+	 * 휴가 대분류 입력 처리
+	 */
+	public void addVacationCategory(VacationCategory vacationCategory) {
+		String vacationCategoryNum = vacationMapper.getCommonNewCode("tb_vacation_category", "vacation_category_num");
+		vacationCategory.setVacationCategoryNum(vacationCategoryNum);
+		vacationMapper.addVacationCategory(vacationCategory);
+	}
+	/**
+	 * 특정 휴가 대분류 수정 
+	 * 
+	 */
+	public VacationCategory getVacationCategoryByNum(String vacationCategoryNum) {
+		VacationCategory vacationCategory = vacationMapper.getVacationCategoryByNum(vacationCategoryNum);
+		return vacationCategory;
+	}
+	/**
+	 * 특정 휴가 대분류 수정 처리
+	 * 
+	 */
+	public void updateVacationCategory(VacationCategory vacationCategory) {
+		vacationMapper.updateVacationCategory(vacationCategory);
+	}
+	/**
+	 * 휴가 중분류 입력 처리
+	 */
+	public void addVacationSort(VacationSort vacationSort) {
+		String vacationSortNum = vacationMapper.getCommonNewCode("tb_vacation_sort", "vacation_sort_num");
+		vacationSort.setVacationSortNum(vacationSortNum);
+		vacationMapper.addVacationSort(vacationSort);
+	}
+	/**
+	 * 특정 휴가 중분류 조회
+	 */
+	public VacationSort getVacationSortByNum(String vacationSortNum) {
+		
+		VacationSort vacationSort = vacationMapper.getVacationSortByNum(vacationSortNum);
+		
+		return vacationSort;
+	}
+	/**
+	 * 특정 휴가 중분류 수정
+	 */
+	public void updateVacationSort(VacationSort vacationSort) {
+		vacationMapper.updateVacationSort(vacationSort);
+	}
+}	
+
