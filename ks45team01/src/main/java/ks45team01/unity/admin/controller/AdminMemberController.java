f92a4a8401fa5aace9b3941f5b5e308f32ac2ce2
ECS <ECS@DESKTOP-048HU3O>
2023-01-10 10:22:38 +0900

    [어드민맴버 컨트롤러 생성 및 어드민 수정화면 추가 사이드바 수정 jwh] 추가 ks45team01/src/main/java/ks45team01/unity/admin/controller/AdminMemberController.java ks45team01/src/main/resources/templates/member/member_admin_modify.html 수정 ks45team01/src/main/resources/templates/member/member_modify.html ks45team01/src/main/resources/templates/member/memberAdmin.html ks45team01/src/main/resources/templates/fragments/sideBar.html

---
+++ ks45team01/src/main/java/ks45team01/unity/admin/controller/AdminMemberController.java
@@ -0,0 +1,78 @@
+package ks45team01.unity.admin.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ks45team01.unity.dto.MemberList;
+import ks45team01.unity.service.MemberListService;
+import ks45team01.unity.service.MemberModifyService;
+
+
+@Controller
+
+@RequestMapping("/AdminMember")
+public class AdminMemberController {
+
+	
+	
+	
+	
+
+	private final MemberModifyService memberModifyService;
+	private final MemberListService memberListService;
+	
+	public AdminMemberController(MemberModifyService memberModifyService
+								,MemberListService memberListService) {
+		
+		
+		this.memberModifyService = memberModifyService;
+		this.memberListService = memberListService;
+	}
+	
+	
+	@GetMapping("/AdminMemberList")
+	public String GetMember(Model model
+						   ,@RequestParam(value="currentPage", required = false, defaultValue="1") int currentPage
+						   ,@RequestParam(value="searchKey", required = false) String searchKey
+						   ,@RequestParam(value="searchValue", required = false, defaultValue = "") String searchValue) {
+		
+		List<MemberList> memberList = memberListService.memberListSe(searchKey, searchValue);
+		Map<String, Object> paramMap = memberListService.getMemberList(currentPage);
+		
+		
+		int lastPage = (int) paramMap.get("lastPage");
+		int startPageNum = (int) paramMap.get("startPageNum");
+		int endPageNum = (int) paramMap.get("endPageNum");
+
+		
+		model.addAttribute("title","사원리스트화면");
+		model.addAttribute("memberList",memberList);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("lastPage", lastPage);
+		model.addAttribute("startPageNum", startPageNum);
+		model.addAttribute("endPageNum", endPageNum);
+	
+		
+		return "member/memberAdmin";
+	}
+	
+	
+	
+	@GetMapping("/MemberAdminModify")
+	public String GetMemberModify(Model model) {
+		
+		List<MemberList> memberModifyList = memberModifyService.memberModifyList();
+		
+		model.addAttribute("title","사원수정화면");
+		model.addAttribute("memberModifyList", memberModifyList);
+	return "member/member_admin_modify";
+	}
+	
+	
+}
