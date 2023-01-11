a7b239c51f738954578072ad2967e0d6351bba51
ECS <ECS@DESKTOP-048HU3O>
2023-01-11 16:01:01 +0900

    [프로젝트 화면 프로젝트인원배정 맵퍼 서비스 추가] 수정 ks45team01/src/main/resources/templates/project/project.html ks45team01/src/main/resources/templates/project/project_member_insert.html ks45team01/src/main/java/ks45team01/unity/worker/controller/ProjectController.java 추가 ks45team01/src/main/java/ks45team01/unity/dto/ProjectMember.java ks45team01/src/main/java/ks45team01/unity/mapper/ProjectMemberInsertMapper.java ks45team01/src/main/resources/mapper/ProjectMemberInsertMapper.xml ks45team01/src/main/java/ks45team01/unity/service/ProjectMemberInsertService.java

---
+++ ks45team01/src/main/java/ks45team01/unity/mapper/ProjectMemberInsertMapper.java
@@ -0,0 +1,23 @@
+package ks45team01.unity.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ks45team01.unity.dto.MemberDepartmentList;
+import ks45team01.unity.dto.MemberList;
+import ks45team01.unity.dto.ProjectMember;
+
+@Mapper
+public interface ProjectMemberInsertMapper {
+	
+	// 인원배정리스트 조인 멤버 조인
+	List<ProjectMember> projectMemberList();
+
+	//부서명 리스트
+	List<MemberDepartmentList> memberDepartmentList();
+	
+	//부서별 사람 리스트 department_list 멤버 조인
+	List<MemberList> memberList();
+}
+
