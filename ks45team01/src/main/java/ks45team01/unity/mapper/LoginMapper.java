e125d8353156043784580a1e466938e435e1be8d
정세현 <@DESKTOP-7I5CCIM>
2023-01-12 14:53:54 +0900

    [커뮤니티 화면 수정 및 로그인 관리 추가 jsh] ks45team01/src/main/resources/templates/community/boardDetail.html ks45team01/src/main/java/ks45team01/unity/worker/controller/LoginController.java ks45team01/src/main/java/ks45team01/unity/mapper/LoginMapper.java ks45team01/src/main/resources/templates/community/noticeDetail.html

--- ks45team01/src/main/java/ks45team01/unity/mapper/LoginMapper.java
+++ ks45team01/src/main/java/ks45team01/unity/mapper/LoginMapper.java
@@ -1,5 +1,19 @@
 package ks45team01.unity.mapper;
 
-public interface LoginMapper {
+import java.util.List;
+import java.util.Map;
 
+import ks45team01.unity.dto.LoginHistory;
+
+public interface LoginMapper {
+	
+	//로그인 이력 조회
+	public List<LoginHistory> getLoginHistory(Map<String, Object> paramMap);
+	//로그인 이력 테이블의 전체 행의 갯수
+	public int getLoginHistoryCnt();
+	
+	//사원 로그인 이력 삭제
+	public int removeLoginHistoryById(String memberNum);
+	
+	
 }
